2dbd263e169478cbe34a39ddd0d53bd66adb12d7
envygod-dev <mbsavage0930@gmail.com>
2024-12-07 20:57:30 +0800

    extract token done

--- microservice/UserAuthService/src/main/java/org/birdnestbackend/userauthservice/config/JwtTokenProvider.java
+++ microservice/UserAuthService/src/main/java/org/birdnestbackend/userauthservice/config/JwtTokenProvider.java
@@ -1,5 +1,6 @@
 package org.birdnestbackend.userauthservice.config;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.birdnestbackend.userauthservice.entity.User;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.List;
 
 @Component
 public class JwtTokenProvider {
@@ -18,12 +20,23 @@ public class JwtTokenProvider {
     private long jwtExpirationInMs;
 
     public String generateToken(User user) {
+        List<String> roles = user.getRoles(); // Fetch roles from the user object
+
         return Jwts.builder()
                 .setSubject(user.getUsername())
-                .claim("roles", user.getRoles()) // Include roles as claims
+                .claim("roles", roles) // Include roles as claims
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationInMs))
                 .signWith(SignatureAlgorithm.HS256, jwtSecret.getBytes()) // Use byte array for key
                 .compact();
+    }
+
+    public List<String> getRolesFromToken(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return (List<String>) claims.get("roles");
     }
 }
